bc40b56b8495911f164cb38ed39546676ace7adf
Hiram Montesillo <montesillo25@gmail.com>
2020-05-30 23:52:36 -0500

    Tipos de datos en Java 10

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -6,5 +6,18 @@ public class DataTypes {
         double nD = 123.456789;
         float nF = 123.234F;
 
+        char c = 'a';
+
+        var salary = 1000;//int
+        //pension 3%
+        var pension = salary * 0.03;//double
+        var totalSalary = salary - pension;
+
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Hiram Montesillo";
+        System.out.println("EMPLOYEE: " + employeeName + " SALARY: " + totalSalary);
     }
 }
